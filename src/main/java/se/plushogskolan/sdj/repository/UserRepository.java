5e74163f0d2d50893c291d00f8f5d64009f1d2b7
Iman Tahriri <iman_tahriri@yahoo.com>
2016-11-07 09:23:36 +0100

    adding main and config files

--- src/main/java/se/plushogskolan/sdj/repository/UserRepository.java
+++ src/main/java/se/plushogskolan/sdj/repository/UserRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
 
+import se.plushogskolan.sdj.model.Team;
 import se.plushogskolan.sdj.model.User;
 
 public interface UserRepository extends CrudRepository<User, Long> {
@@ -11,5 +12,6 @@ public interface UserRepository extends CrudRepository<User, Long> {
 	User findByUsername(String username);
 	List<User> findByFirstname(String firstname);
 	List<User> findByLastname(String lastname);
+//	List<User> getAllUsersInTeam(Team team);
 	
 }
