2c21b00354dc4486d8c1a96cad3e59b7f87f5844
IrinaMix <Irina@joakims-laptop.learnet.se>
2016-11-07 10:38:03 +0100

    add team service

--- src/main/java/se/plushogskolan/sdj/service/TeamService.java
+++ src/main/java/se/plushogskolan/sdj/service/TeamService.java
@@ -12,11 +12,13 @@ import se.plushogskolan.sdj.model.Status;
 import se.plushogskolan.sdj.model.Team;
 import se.plushogskolan.sdj.model.User;
 import se.plushogskolan.sdj.repository.TeamRepository;
+import se.plushogskolan.sdj.repository.UserRepository;
 
 @Service
 public class TeamService {
 
-	private final TeamRepository teamRepository;
+	private TeamRepository teamRepository;
+	private UserRepository userRepository;
 
 	@Autowired
 	public TeamService(TeamRepository teamRepository) {
@@ -25,36 +27,91 @@ public class TeamService {
 
 	@Transactional
 	public Team addTeam(Team team) {
-//		Team team1 = teamRepository.byName(team.getName());
-//		System.out.println(team1);
-//		
-		return	teamRepository.save(team);
-		
+		try {
+			if (!teamRepository.exists(team.getId())) {
+				return teamRepository.save(team);
+
+			} else {
+				throw new ServiceException("Team with this teamname already exists");
+			}
+
+		} catch (Exception e) {
+			throw new ServiceException("Could not add team: " + team.getName(), e);
+		}
 	}
 
 	public Iterable<Team> findAllTeams() {
-		return teamRepository.findAll();
+		try {
+			return teamRepository.findAll();
+		} catch (Exception e) {
+			throw new ServiceException("Could not get all teams ", e);
+		}
 	}
 
 	@Transactional
-	public void uppdateTeam(String teamName, String newName) {
-		Team team = teamRepository.findByName(teamName);
-		team.setName(newName);
-		teamRepository.save(team);
+	public void uppdateTeam(String oldName, String newName) {
+		try {
+
+			if (teamRepository.exists(teamRepository.findByName(oldName).getId())) {
+				if (!teamRepository.exists(teamRepository.findByName(newName).getId())) {
+					Team team = teamRepository.findByName(oldName);
+					team.setName(newName);
+					teamRepository.save(team);
+				} else {
+					throw new ServiceException("Team with this teamname " + newName + " exists");
+
+				}
+			} else {
+				throw new ServiceException("Team with this teamname NOT exists");
+			}
+
+		} catch (Exception e) {
+			throw new ServiceException("Could not update team", e);
+		}
 	}
 
 	@Transactional
 	public void deactivateTeam(String teamName) {
-		Team team = teamRepository.findByName(teamName);
-		team.setStatus(Status.INACTIVE.toString());
-		teamRepository.save(team);
+
+		try {
+
+			if (teamRepository.exists(teamRepository.findByName(teamName).getId())) {
+				Team team = teamRepository.findByName(teamName);
+				team.setStatus(Status.INACTIVE.toString());
+				teamRepository.save(team);
+			} else {
+				throw new ServiceException("Team with this teamname NOT exists");
+			}
+
+		} catch (Exception e) {
+			throw new ServiceException("Could not deactivate team: " + teamName, e);
+		}
+
 	}
 
-	@Transactional
-	public void assigneUserToTeam(String teamName, User user) {
-		Team team = teamRepository.findByName(teamName);
-		//team.setStatus("Inactive");
-		teamRepository.save(team);
-	}
+//	@Transactional
+//	public void assigneUserToTeam(String teamName, String userId) {
+//		try {
+//			if (teamRepository.exists(teamRepository.findByName(teamName).getId())) {
+//
+//				List<User> users = userRepository.findAllByTeam(teamRepository.findByName(teamName));
+//				if (users.size() < 10) {
+//					User user1 = userRepository.;
+//					team.setStatus(Status.INACTIVE.toString());
+//					this.userRepository;
+//
+//				} else {
+//					throw new ServiceException(
+//							"This team already has 10 users! (But it is allowed to have MAX 10 users in one team)");
+//				}
+//
+//			} else {
+//				throw new ServiceException("Team with this teamId NOT exists");
+//			}
+//
+//		} catch (Exception e) {
+//			throw new ServiceException("Could not update team", e);
+//		}
+//	}
 
 }
