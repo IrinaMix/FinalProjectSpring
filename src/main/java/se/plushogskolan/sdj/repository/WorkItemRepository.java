cca9376dbbaf26cb08dd0e1588b5fdf4d943b39a
Mathias Hultman <hultman@mensa.se>
2016-11-07 12:41:37 +0100

    made it cleaner, otherwise no update

--- src/main/java/se/plushogskolan/sdj/repository/WorkItemRepository.java
+++ src/main/java/se/plushogskolan/sdj/repository/WorkItemRepository.java
@@ -1,6 +1,5 @@
 package se.plushogskolan.sdj.repository;
 
-
 import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 import org.springframework.data.repository.CrudRepository;
@@ -8,23 +7,19 @@ import org.springframework.data.repository.query.Param;
 
 import se.plushogskolan.sdj.model.User;
 import se.plushogskolan.sdj.model.WorkItem;
-import java.util.List;
 
 public interface WorkItemRepository extends CrudRepository<WorkItem, Long> {
 
-    List<WorkItem> findAllByStatus(String status);
+	List<WorkItem> findAllByStatus(String status);
 
-    	@Query("select w from WorkItem w where w.user.team.name=:teamName")
-    List<WorkItem> findAllByTeamName(@Param("teamName") String teamName);
+	List<WorkItem> findAllByUser(String userId);
 
-    List<WorkItem> findAllByUser(String userId);
-    List<WorkItem> findAllByUser(User user);
+	List<WorkItem> findAllByUser(User user);
 
-    //I get an error on the first wi, is not expected. Why is this?
-//    @Query("select wi from #{#entityName} wi where wi.number like :number")
-//    List<WorkItem> findAllByText(@Param("number") String num);
+	List<WorkItem> findByDescriptionContaining(String text);
 
-    List<WorkItem> findByDescriptionContaining(String text);
-    List<WorkItem> findByTitleContaining(String text);
-    
+	List<WorkItem> findByTitleContaining(String text);
+
+	@Query("select w from WorkItem w where w.user.team.name=:teamName")
+	List<WorkItem> findAllByTeamName(@Param("teamName") String teamName);
 }
